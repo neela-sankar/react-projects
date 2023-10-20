6d6b62531357d84eeb493eb4dd534a67646538f5
neela-sankar <neela.ecest@gmail.com>
2023-10-20 11:39:16 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default[
+    {
+        id: 1,
+        name: 'Bertie Yates',
+        age: 29,
+        image:
+          'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg',
+      },
+      {
+        id: 2,
+        name: 'Hester Hogan',
+        age: 32,
+        image:
+          'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg',
+      },
+      {
+        id: 3,
+        name: 'Larry Little',
+        age: 36,
+        image:
+          'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg',
+      },
+      {
+        id: 4,
+        name: 'Sean Walsh',
+        age: 34,
+        image:
+          'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg',
+      },
+      {
+        id: 5,
+        name: 'Lola Gardner',
+        age: 29,
+        image:
+          'https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg',
+      },
+]
